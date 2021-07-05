f7d02c710164a76188a874bf17e6301f301c8894
distorition <85959781+distorition@users.noreply.github.com>
2021-07-05 21:05:18 +0300

    Add files via upload

---
+++ Lesson 6.1/Lesson 6.1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] proces = System.Diagnostics.Process.GetProcesses();
+            
+            foreach (var pro in proces)
+            {
+                Console.WriteLine($"{pro}: Ид процесса {pro.Id}\n");
+            }
+            int a=Convert.ToInt32(Console.ReadLine());
+           
+                try
+                {
+                    Process();
+                }
+                catch
+                {
+                    Console.WriteLine("введено неверное значения ");
+                    Process();
+                }
+      
+          
+          
+
+
+
+
+
+
+
+
+            //int num;
+            //Console.WriteLine("Ввидите имя процесса или его номер который хотите закрыть ");
+
+            //string str = Console.ReadLine();
+            //bool aaa = int.TryParse(str, out num);
+            //if (aaa == true)
+            //{
+              
+            //}
+            //else if (aaa == false)
+            //{
+                
+
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("такого процесса нету");
+            //}
+
+
+
+
+
+
+
+
+        }
+
+        private static void Process()
+        {
+            Console.WriteLine("как хотите закрыть процесс ?");
+            Console.WriteLine("1- при помощи имени процесса");
+            Console.WriteLine("2- при помощи его ид");
+            int choise = Convert.ToInt32(Console.ReadLine());
+            if (choise == 1)
+            {
+                Console.WriteLine("Ввидите имя процесса");
+
+                string name = Console.ReadLine();
+                Process process = System.Diagnostics.Process.Start(name);
+                process.Kill();
+
+                Console.WriteLine(process.ExitCode);
+            }
+            if (choise == 2)
+            {
+                Console.WriteLine("Ввидите Ид процесса");
+                int a1 = Convert.ToInt32(Console.ReadLine());
+
+
+                Process process1 = System.Diagnostics.Process.GetProcessById(a1);
+                process1.Kill();
+                Console.WriteLine(process1.ExitCode);
+            }
+            else
+            {
+                Console.WriteLine("выбирите конкретно между 1 и 2");
+            }
+        }
+    }
+}
